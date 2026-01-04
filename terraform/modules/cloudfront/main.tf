f515c4376d94f7b78640cf5cfd124d1effe0b847
sajil <sajil>
2026-01-04 12:29:55 +0530

    added waf protection

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -22,6 +22,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_root_object = "index.html"
 
   aliases = ["${var.my_domain}"]
+  web_acl_id = aws_wafv2_web_acl.acfp-example.arn
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -156,5 +157,73 @@ resource "aws_route53_record" "cloudfront" {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
+  }
+}
+
+resource "aws_wafv2_web_acl" "acfp-example" {
+  name        = "managed-acfp-web-acl"
+  description = "Example of a managed ACFP rule."
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "acfp-rule-1"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesACFPRuleSet"
+        vendor_name = "AWS"
+
+        managed_rule_group_configs {
+          aws_managed_rules_acfp_rule_set {
+            creation_path          = "/signin"
+            registration_page_path = "/register"
+
+            request_inspection {
+              email_field {
+                identifier = "/email"
+              }
+
+              password_field {
+                identifier = "/password"
+              }
+
+              payload_type = "JSON"
+
+              username_field {
+                identifier = "/username"
+              }
+            }
+
+            response_inspection {
+              status_code {
+                failure_codes = ["403"]
+                success_codes = ["200"]
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
   }
 }
